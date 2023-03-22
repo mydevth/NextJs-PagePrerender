ebf171ae5c7ea60763a7fc3f82774734b6561a8d
mydevth <mydevth@chonburi.biz>
2023-03-22 22:59:44 +0700

    section04

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import path from "path";
+// import fs from "fs";
+
+// import { promise as fs } from "fs";
+
+function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps(context) {
+  console.log("(Re-) Generation...");
+  const fs = require("fs/promises");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
